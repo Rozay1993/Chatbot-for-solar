a64c98be0ed116bf8c7215f58755864a0a85024d
peakdev118 <peakdev118@gmail.com>
2023-02-28 09:51:44 -0500

    convert to pinecone

--- backend/chat_api/views.py
+++ backend/chat_api/views.py
@@ -3,29 +3,33 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
-
 import os
-from os.path import exists
-import pandas as pd
+from tqdm.auto import tqdm 
+# moduld related with openai
 import openai
-from openai.embeddings_utils import distances_from_embeddings
-import numpy as np
-from openai.embeddings_utils import distances_from_embeddings, cosine_similarity
+# pinecone module
+import pinecone
+# read env file
 from dotenv import load_dotenv
+#get api key of openai from .env
 load_dotenv()
+
+pine_cone_api_key = os.getenv("PINE_CONE_API_KEY")
+pine_cone_environment = os.getenv("PINE_CONE_ENVIRONMENT")
+print("pine_cone_api_key: ",pine_cone_api_key)
+print("pine_cone_environment: ",pine_cone_environment)
+
 
 openai_api_key = os.getenv("OPENAI_API_KEY")
 print("openai_api_key_____",openai_api_key)
 openai.api_key=openai_api_key
 
-file_exists = exists('processed/embeddings.csv')
-df=pd.DataFrame()
-print(file_exists)
-if(file_exists ==True):
-    df=pd.read_csv('processed/embeddings.csv', index_col=0)
-    print('end read')
-    df['embeddings'] = df['embeddings'].apply(eval).apply(np.array)
-    
+MODEL = 'text-embedding-ada-002'
+
+pinecone.init(api_key=pine_cone_api_key, environment=pine_cone_environment)
+
+pine_index = pinecone.Index("podiodata")
+
 print('start chatbot')
 class Chatbot(APIView):
     # add permission to check if user is authenticated
@@ -34,52 +38,35 @@ class Chatbot(APIView):
     def post(self, request, *args, **kwargs):
         question =  request.data.get('question'), 
         print(question)
-        answer = answer_question(df, question=question, debug=False)
+        answer = answer_question(question=question, debug=False)
         send_data= {}
         send_data["answer"] = answer
         return Response(send_data, status=status.HTTP_201_CREATED)
 
-def create_context(
-    question, df, max_len=1800, size="ada"
-):
+def create_context(question):
     """
     Create a context for a question by finding the most similar context from the dataframe
     """
 
     # Get the embeddings for the question
-    q_embeddings = openai.Embedding.create(input=question, engine='text-embedding-ada-002')['data'][0]['embedding']
+    xq  = openai.Embedding.create(input=question, engine=MODEL)['data'][0]['embedding']
 
-    # Get the distances from the embeddings
-    df['distances'] = distances_from_embeddings(q_embeddings, df['embeddings'].values, distance_metric='cosine')
-
+    res = pine_index.query([xq], top_k=2, include_metadata=True)
 
     returns = []
-    cur_len = 0
 
-    # Sort by distance and add the text to the context until the context is too long
-    for i, row in df.sort_values('distances', ascending=True).iterrows():
-        
-        # Add the length of the text to the current length
-        cur_len += row['n_tokens'] + 4
-        
-        # If the context is too long, break
-        if cur_len > max_len:
-            break
-        
-        # Else add it to the text that is being returned
-        returns.append(row["text"])
+    for match in res['matches']:
+        returns.append(match['metadata']["text"])
 
     # Return the context
     return "\n\n###\n\n".join(returns)
 
+
 def answer_question(
-    df,
     model="text-davinci-003",
     question="Am I allowed to publish model outputs to Twitter, without a human review?",
-    max_len=1800,
-    size="ada",
     debug=False,
-    max_tokens=150,
+    max_tokens=1000,
     stop_sequence=None
 ):
     """
@@ -87,9 +74,6 @@ def answer_question(
     """
     context = create_context(
         question,
-        df,
-        max_len=max_len,
-        size=size,
     )
     # If debug, print the raw model response
     if debug:
@@ -99,7 +83,7 @@ def answer_question(
     try:
         # Create a completions using the questin and context
         response = openai.Completion.create(
-            prompt=f"Answer the question based on the context below, and if the question can't be answered based on the context, say \"Excuse me, but please inquire only about the website.\"\n\nContext: {context}\n\n---\n\nQuestion: {question}\nAnswer:",
+            prompt=f"Answer the question based on the context below.\"\n\nContext: {context}\n\n---\n\nQuestion: {question}\nAnswer:",
             temperature=0,
             max_tokens=max_tokens,
             top_p=1,
