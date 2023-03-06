5b33f5cd60d5d9163b271743ac78bd4c8714cc1f
peakdev118 <peakdev118@gmail.com>
2023-03-06 18:37:36 -0500

    podio add

--- backend/chat_api/views.py
+++ backend/chat_api/views.py
@@ -9,6 +9,9 @@ from tqdm.auto import tqdm
 import openai
 # pinecone module
 import pinecone
+#podio module
+from  .pypodio2 import api as podio_api
+
 # read env file
 from dotenv import load_dotenv
 #get api key of openai from .env
@@ -29,6 +32,13 @@ MODEL = 'text-embedding-ada-002'
 pinecone.init(api_key=pine_cone_api_key, environment=pine_cone_environment)
 
 pine_index = pinecone.Index("podiodata")
+#############################
+client_id=os.getenv("PODIO_CLIENT_ID")
+client_secret=os.getenv("PODIO_CLIENT_SECRET")
+app_id=os.getenv("PODIO_APP_ID")
+app_token=os.getenv("PODIO_APP_TOKEN")
+
+podio = podio_api.OAuthAppClient(client_id,client_secret,app_id,app_token)
 
 print('start chatbot')
 class Chatbot(APIView):
@@ -41,6 +51,17 @@ class Chatbot(APIView):
         send_data= {}
         send_data["answer"] = answer
         return Response(send_data, status=status.HTTP_201_CREATED)
+
+class PodioHook(APIView):
+    def post(self, request, *args, **kwargs):
+        request = request.data
+        print(request)
+    def put(self, request, *args, **kwargs):
+        request = request.data
+        print(request)
+    def delete(self, request, *args, **kwargs):
+        request = request.data
+        print(request)
 
 def create_context(question):
     """
