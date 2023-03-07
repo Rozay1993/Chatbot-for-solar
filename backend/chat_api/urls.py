061e316e907d75c9d075cd78c4653b91792f2aa1
peakdev118 <peakdev118@gmail.com>
2023-03-06 20:48:24 -0500

    add url

--- backend/chat_api/urls.py
+++ backend/chat_api/urls.py
@@ -2,8 +2,10 @@
 from django.urls import path, include
 from .views import (
     Chatbot,
+    PodioHook
 )
 
 urlpatterns = [
     path('chat', Chatbot.as_view()),
+    path('podio', PodioHook.as_view()),
 ]
