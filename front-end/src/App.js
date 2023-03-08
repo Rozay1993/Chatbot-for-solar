3439865bf0bb754c6b8c219234fccd8bd2840b2a
peakdev118 <peakdev118@gmail.com>
2023-03-08 16:49:59 -0500

    modify front-end

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -86,12 +86,12 @@ function App() {
 	// };
 
 	return (
-		<div className="flex flex-col items-center justify-center w-screen min-h-screen bg-gray-100 text-gray-800 p-10">
-			<div className="flex flex-col flex-grow w-full max-w-xl bg-white shadow-xl rounded-lg overflow-hidden">
+		<div className="flex flex-col items-center justify-center w-screen min-h-screen bg-gray-100 text-gray-800 p-10 relative">
+			<div className="md:flex md:flex-col flex-grow w-full md:max-w-[40%] bg-white shadow-xl rounded-lg overflow-hidden order-2">
 				<div className="flex justify-center border-b-2 py-2">
 					<img className="w-[30%]" src={LogoIcon} alt="logo"></img>
 				</div>
-				<div className="flex flex-col flex-grow h-0 p-4 overflow-auto">
+				<div className="flex flex-col flex-grow p-4 overflow-auto md:h-0 h-[calc(100vh-14rem)]">
 					<ChatComponent
 						humanChat={false}
 						chatContent={
@@ -119,7 +119,7 @@ function App() {
 						</div>
 					) : null}
 				</div>
-				<div className="bg-gray-300 p-4">
+				<div className="bg-gray-300 p-4 ">
 					<input
 						className="flex items-center h-10 w-full rounded px-3 text-sm"
 						type="text"
@@ -128,6 +128,16 @@ function App() {
 						onKeyUp={onMessageKeyUp}
 						value={chatting_data}
 					/>
+				</div>
+			</div>
+			<div className="relative md:absolute text-center font-bold md:text-xl text-base md:top-[0px] md:right-[0px] md:w-[30vw] md:p-10 pb-10 order-1 items-center md:h-screen flex">
+				<div className="w-full">
+					<p>This is a Beta product Powered by AI. </p>
+					<p>As of right now it will only give the "Stage" of the Porject </p>
+					<p className="w-[80%] mx-auto md:mt-20 mt-5">
+						Questions must be frased as the following- "What is teh stage of for
+						customer X?"
+					</p>
 				</div>
 			</div>
 		</div>
